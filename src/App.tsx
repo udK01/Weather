6453e8f6a39ace9faa6f7540bb79fa7a25022fbf
udK01 <dominik.muranyi@gmail.com>
2024-12-25 14:41:48 +0000

    Added fetch weather function with API key in .env file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [city, setCity] = useState("Swansea");
+
+  const fetchWeather = async (city: string): Promise<void> => {
+    try {
+      const API_KEY = import.meta.env.VITE_API_KEY;
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchWeather(city);
+  }, [city]);
+
   return (
-    <div className="text-bold bg-red-500 text-center text-[32px] text-white h-screen">
-      Hello World!
+    <div>
+      <h1>Weather in {city}</h1>
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        placeholder="Enter city"
+      />
     </div>
   );
 }
